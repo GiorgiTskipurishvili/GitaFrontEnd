ec1d42521777e0e63dfe2b3ccc36aab8f0510739
GiorgiTskipurishvili <giorgitskipurishvili@gmail.com>
2026-06-24 23:51:33 +0400

    feat(homework) add homework 15

--- HomeWork/15_homework/src/components/AccordionCard.jsx
+++ HomeWork/15_homework/src/components/AccordionCard.jsx
@@ -3,6 +3,12 @@ import { useState } from "react";
 import { items } from "../../item";
 import AccordionItem from "./AccordionItem";
 import Logo from "../assets/logo.svg";
+import Box from "../assets/box.svg";
+import Shadow from "../assets/shadow.svg";
+import FirstLiner from "../assets/liner1.svg";
+import SecondLiner from "../assets/liner2.svg";
+import LogoMobile from "../assets/logo-mobile.svg";
+import ShadowMobile from "../assets/shadow-mobile.svg";
 
 export default function AccordionCard() {
   const [activeAccordionNumber, setActiveAccordionNumber] = useState(2);
@@ -17,10 +23,20 @@ export default function AccordionCard() {
 
   return (
     <main className={styles.AccordionCard}>
-      <div>
-        <img src={Logo} alt="hero" />
+      <div className={styles.logoWrapper}>
+        <img className={styles.ShadowImg} src={Shadow} alt="shadow" />
+        <img className={styles.FirstLinerImg} src={FirstLiner} alt="firstliner" />
+        <img className={styles.SecondLinerImg} src={SecondLiner} alt="secondliner" />
+
+        <img className={styles.LogoImg} src={Logo} alt="logo" />
+        <img className={styles.BoxImg} src={Box} alt="box" />
+
+        <img className={styles.LogoMobileImg} src={LogoMobile} />
+        <img className={styles.ShadowMobileImg} src={ShadowMobile} />
+
       </div>
       <div className={styles.item_container}>
+        <h1 className={styles.faq}>FAQ</h1>
         {items.map((item) => (
           <AccordionItem
             key={item.randomNum}
--- HomeWork/15_homework/src/components/AccordionItem.jsx
+++ HomeWork/15_homework/src/components/AccordionItem.jsx
@@ -1,12 +1,14 @@
 
 import styles from "./AccordionItem.module.css"
+import DropDown from "../assets/dropdown.svg";
 export default function AccordionItem({title,desc,randomNum,activeAccordionNumber,handleClick}) {
   const isAccordionItemH2BoldActive = activeAccordionNumber === randomNum;
+  const isAccordingItemDropDownRotate = activeAccordionNumber === randomNum;
   return (
     <div className={styles.AccordionItem} >
       <h2 className={`${styles.AccordionItemH2} ${isAccordionItemH2BoldActive ? styles.AccordionItemH2BoldActive : null}`} onClick={() => {
         handleClick(randomNum)
-      }}>{title}</h2>
+      }}>{title} <img className={`${isAccordingItemDropDownRotate ? styles.AccordingItemDropDownRotate : null}`} src={DropDown} alt="dropdown" /></h2>
 
       {
         activeAccordionNumber === randomNum ? <p className={styles.AccordionItemP}>{desc}</p>: null
