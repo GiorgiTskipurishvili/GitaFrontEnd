7bee02cd18ae3edfd414139f6497d7c7fd16e0ac
GiorgiTskipurishvili <giorgitskipurishvili@gmail.com>
2026-07-19 00:19:51 +0400

    responsive design with tailwindcss

--- HomeWork/22_homework/src/components/footer/Footer.tsx
+++ HomeWork/22_homework/src/components/footer/Footer.tsx
@@ -8,36 +8,32 @@ import Facebook from "../../assets/facebook.svg"
 export default function Footer() {
   return (
     <>
-      <footer className="bg-[#020202]">
-        <section className="max-w-360 mx-auto px-35 m-auto flex justify-between items-center py-22.75">
-          <div className="flex gap-10">
-            <div><img src={Logo} alt="" /></div>
-            <div>
-                <div>
-                    <ul className="flex gap-6 text-[#B1B1B1] text-[16px] font-medium">
-                        <li>CONTATO</li>
-                        <li>SOBRE</li>
-                        <li>sUPORTE</li>
-                        <li>EMPREGOS</li>
-                    </ul>
-                </div>
-                <div>
-                    <span className="text-[13px] font-light text-[#B1B1B1]">©2023 Rockstar Games, Inc.    -    YWR Studio.  </span>
-                </div>
-            </div>
-          </div>
-
+    <footer className="bg-[#020202]">
+      <section className="max-w-360 mx-auto px-4 sm:px-8 lg:px-35 py-10 sm:py-14 lg:py-22.75 flex flex-col lg:flex-row items-center lg:items-center justify-between gap-8">
+        <div className="flex flex-col sm:flex-row items-center gap-6 sm:gap-10 text-center sm:text-left">
+          <img src={Logo} alt="" className="h-6 sm:h-auto" />
           <div>
-            <ul className="flex gap-8">
-                <li><img src={Twitch} alt="twitch" /></li>
-                <li><img src={Instagram} alt="instagram" /></li>
-                <li><img src={Twitter} alt="twitter" /></li>
-                <li><img src={Youtube} alt="youtube" /></li>
-                <li><img src={Facebook} alt="facebook" /></li>
+            <ul className="flex flex-wrap justify-center sm:justify-start gap-4 lg:gap-6 text-[#B1B1B1] text-sm lg:text-[16px] font-medium">
+              <li>CONTATO</li>
+              <li>SOBRE</li>
+              <li>SUPORTE</li>
+              <li>EMPREGOS</li>
             </ul>
+            <span className="block mt-2 sm:mt-0 text-xs lg:text-[13px] font-light text-[#B1B1B1]">
+              ©2023 Rockstar Games, Inc. - YWR Studio.
+            </span>
           </div>
-        </section>
-      </footer>
+        </div>
+
+        <ul className="flex gap-6 lg:gap-8">
+          <li><img src={Twitch} alt="twitch" className="h-5 lg:h-auto" /></li>
+          <li><img src={Instagram} alt="instagram" className="h-5 lg:h-auto" /></li>
+          <li><img src={Twitter} alt="twitter" className="h-5 lg:h-auto" /></li>
+          <li><img src={Youtube} alt="youtube" className="h-5 lg:h-auto" /></li>
+          <li><img src={Facebook} alt="facebook" className="h-5 lg:h-auto" /></li>
+        </ul>
+      </section>
+    </footer>
     </>
   );
 }
--- HomeWork/22_homework/src/components/header/Header.tsx
+++ HomeWork/22_homework/src/components/header/Header.tsx
@@ -5,34 +5,34 @@ import DropDown from "../../assets/arrow_drop_down.svg";
 export default function Header() {
   return (
     <>
-      <header className="bg-[#020202]">
-        <section className="max-w-360 px-35  m-auto flex justify-between items-center py-7.25">
-          <div className="flex items-center">
-            <div className="mr-11.75">
-              <img src={Logo} alt="logo" />
-            </div>
-            <div>
-              <ul className="text-[white] flex gap-6 text-[16px]">
-                <li className="flex">
-                  Jogos <img src={DropDown} alt="dropDown" />
-                </li>
-                <li>Loja</li>
-                <li>Ajuda</li>
-              </ul>
-            </div>
+    <header className="bg-[#020202]">
+      <section className="max-w-360 mx-auto px-4 sm:px-6 md:px-10 lg:px-35 py-4 md:py-6 lg:py-7.25 flex flex-wrap items-center justify-between gap-4">
+        <div className="flex items-center gap-4 md:gap-8 lg:gap-0">
+          <div className="lg:mr-11.75">
+            <img src={Logo} alt="logo" className="h-6 sm:h-7 lg:h-auto" />
           </div>
+          <div className="hidden md:block">
+            <ul className="text-white flex gap-4 lg:gap-6 text-sm lg:text-[16px]">
+              <li className="flex items-center gap-1">
+                Jogos <img src={DropDown} alt="dropDown" />
+              </li>
+              <li>Loja</li>
+              <li>Ajuda</li>
+            </ul>
+          </div>
+        </div>
 
-          <div className="flex items-center gap-10">
-            <div>
-              <button className="text-[#020202] text-[20px] bg-[#FCAF17] rounded-lg px-4 py-2.75 cursor-pointer">Baixe o Launcher</button>
-            </div>
-            <div className="flex gap-6">
-              <img src={Search} alt="search" />
-                <img src={User} alt="user" />
-            </div>
+        <div className="flex items-center gap-3 sm:gap-5 lg:gap-10 order-3 md:order-none w-full md:w-auto justify-between md:justify-end">
+          <button className="text-[#020202] text-sm sm:text-base lg:text-[20px] bg-[#FCAF17] rounded-lg px-3 sm:px-4 py-2 lg:py-2.75 cursor-pointer whitespace-nowrap">
+            Baixe o Launcher
+          </button>
+          <div className="flex gap-4 lg:gap-6">
+            <img src={Search} alt="search" className="h-5 lg:h-auto" />
+            <img src={User} alt="user" className="h-5 lg:h-auto" />
           </div>
-        </section>
-      </header>
+        </div>
+      </section>
+    </header>
     </>
   );
 }
